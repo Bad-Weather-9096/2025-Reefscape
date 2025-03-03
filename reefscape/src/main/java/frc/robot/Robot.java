b77a2e24c3d16008553a1452b6d40a6bc9877823
Greg Brown <gk_brown@icloud.com>
2025-03-03 11:15:43 -0500

    Update tests.

--- reefscape/src/main/java/frc/robot/Robot.java
+++ reefscape/src/main/java/frc/robot/Robot.java
@@ -106,14 +106,14 @@ public class Robot extends TimedRobot {
         var ht = type.getHeight().in(Units.Meters) + Units.Inches.of(TAG_HEIGHT).in(Units.Meters) / 2;
         var hc = Units.Inches.of(BASE_HEIGHT + cameraHeight).in(Units.Meters);
 
-        var a = Math.toRadians(heading - target.getAngle().in(Units.Degrees));
+        var a = Math.toRadians(target.getAngle().in(Units.Degrees) - heading);
 
         var dx = (ht - hc) / Math.tan(Math.toRadians(ty));
 
         var dy1 = dx * Math.tan(a + Math.toRadians(tx));
         var dy2 = Units.Inches.of(cameraOffset).in(Units.Meters) * Math.sin(a);
 
-        return new AlignmentParameters(-a, dy1 + dy2);
+        return new AlignmentParameters(a, dy1 + dy2);
     }
 
     @Override
--- reefscape/src/test/java/frc/robot/RobotTest.java
+++ reefscape/src/test/java/frc/robot/RobotTest.java
@@ -16,13 +16,13 @@ public class RobotTest {
     @Test
     public void testAlignmentParameters() {
         logAlignmentParameters(getTarget(1), -135.0, 10.0, 7.5);
-        logAlignmentParameters(getTarget(1), -180.0, 5.5, 12.5);
-        logAlignmentParameters(getTarget(1), 180.0, 5.5, 12.5);
-
         logAlignmentParameters(getTarget(2), 135.0, -10.0, 7.5);
 
         logAlignmentParameters(getTarget(7), 30.0, -12.5, -7.5);
         logAlignmentParameters(getTarget(7), -30.0, 12.5, -7.5);
+
+        logAlignmentParameters(getTarget(1), -180.0, 5.5, 12.5);
+        logAlignmentParameters(getTarget(1), 180.0, 5.5, 12.5);
     }
 
     private void logAlignmentParameters(FieldElement target, double heading, double tx, double ty) {
