20f48ce267e93f4ff8dbe8c0555984fbce813fc6
Greg Brown <gk_brown@icloud.com>
2025-03-20 08:57:07 -0400

    Update intake code.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -10,11 +10,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
     private SparkMax elevatorSparkMax = new SparkMax(9, SparkLowLevel.MotorType.kBrushless);
-    private SparkMax intakeSparkMax = new SparkMax(10, SparkLowLevel.MotorType.kBrushless);
+
+    private SparkMax intakeSparkMax1 = new SparkMax(10, SparkLowLevel.MotorType.kBrushless);
+    private SparkMax intakeSparkMax2 = new SparkMax(11, SparkLowLevel.MotorType.kBrushless);
 
     private static final double CORAL_LENGTH = 11.875; // inches
     private static final double INTAKE_WHEEL_DIAMETER = 2.25; // inches
-    private static final double INTAKE_GEAR_RATIO = 1.0;
 
     public ElevatorSubsystem() {
         var elevatorConfig = new SparkMaxConfig();
@@ -45,9 +46,17 @@ public class ElevatorSubsystem extends SubsystemBase {
             .d(0)
             .outputRange(-1, 1);
 
-        intakeSparkMax.configure(intakeConfig,
+        intakeSparkMax1.configure(intakeConfig,
             SparkBase.ResetMode.kResetSafeParameters,
             SparkBase.PersistMode.kPersistParameters);
+
+        intakeSparkMax1.getEncoder().setPosition(0);
+
+        intakeSparkMax2.configure(intakeConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters);
+
+        intakeSparkMax2.getEncoder().setPosition(0);
     }
 
     public void setElevatorPosition(ElevatorPosition elevatorPosition) {
@@ -67,16 +76,20 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     private void moveCoral(double multiplier) {
-        var position = intakeSparkMax.getEncoder().getPosition();
-
         var rotations = CORAL_LENGTH / (INTAKE_WHEEL_DIAMETER * Math.PI) * multiplier;
 
-        intakeSparkMax.getClosedLoopController().setReference(position + rotations * INTAKE_GEAR_RATIO, SparkBase.ControlType.kPosition);
+        intakeSparkMax1.getClosedLoopController().setReference(intakeSparkMax1.getEncoder().getPosition() + rotations,
+            SparkBase.ControlType.kPosition);
+
+        intakeSparkMax2.getClosedLoopController().setReference(intakeSparkMax2.getEncoder().getPosition() - rotations,
+            SparkBase.ControlType.kPosition);
     }
 
     @Override
     public void periodic() {
         SmartDashboard.putNumber("elevator-position", elevatorSparkMax.getEncoder().getPosition());
-        SmartDashboard.putNumber("intake-position", intakeSparkMax.getEncoder().getPosition());
+
+        SmartDashboard.putNumber("intake1-position", intakeSparkMax1.getEncoder().getPosition());
+        SmartDashboard.putNumber("intake2-position", intakeSparkMax2.getEncoder().getPosition());
     }
 }
